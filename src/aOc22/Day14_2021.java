22e6b6159e1f09bccce42b0d7300f9c31bd2f2af
Pucchosa <cdea89@gmail.com>
2022-11-01 08:50:01 +0100

    new credentials

---
+++ src/aOc22/Day14_2021.java
@@ -0,0 +1,25 @@
+package aOc22;
+
+import java.util.ArrayList;
+
+public class Day14_2021 {
+	public static long calculate1(String s) {
+		ArrayList<String[]> lista=Read.copList(s);
+		int fin=0;
+		
+		
+		
+		
+		return (long) fin;
+	}
+	public static long calculate2(String s) {
+		ArrayList<String[]> lista=Read.copList(s);
+		int fin=0;
+		
+		
+		
+		
+		
+		return (long) fin;
+	}
+}
