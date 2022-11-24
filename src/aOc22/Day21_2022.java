f66dbfe6d7f6ebc5585baef8e75e47cfd3c5de98
Pucchosa <pucchosa@proton.me>
2022-11-24 09:48:46 +0100

    more preparations for 2022

---
+++ src/aOc22/Day21_2022.java
@@ -0,0 +1,22 @@
+package aOc22;
+
+import program.Nope;
+
+public class Day21_2022 {
+	public static long calculate1(String s) {
+		int fin=0;
+		
+		Nope no=new Nope("Sorry, develop time travel is way beyond my capacity!");
+		no.setVisible(true);
+		
+		return (long) fin;
+	}
+	public static long calculate2(String s) {
+		int fin=0;
+		
+		Nope no=new Nope("Sorry, develop time travel is way beyond my capacity!");
+		no.setVisible(true);
+		
+		return (long) fin;
+	}
+}
--- src/program/Handler.java
+++ src/program/Handler.java
@@ -25,6 +25,30 @@ import aOc21.Day23_2021;
 import aOc21.Day24_2021;
 import aOc21.Day25_2021;
 import aOc22.Day01_2022;
+import aOc22.Day02_2022;
+import aOc22.Day03_2022;
+import aOc22.Day04_2022;
+import aOc22.Day05_2022;
+import aOc22.Day06_2022;
+import aOc22.Day07_2022;
+import aOc22.Day08_2022;
+import aOc22.Day09_2022;
+import aOc22.Day10_2022;
+import aOc22.Day11_2022;
+import aOc22.Day12_2022;
+import aOc22.Day13_2022;
+import aOc22.Day14_2022;
+import aOc22.Day15_2022;
+import aOc22.Day16_2022;
+import aOc22.Day17_2022;
+import aOc22.Day18_2022;
+import aOc22.Day19_2022;
+import aOc22.Day20_2022;
+import aOc22.Day21_2022;
+import aOc22.Day22_2022;
+import aOc22.Day23_2022;
+import aOc22.Day24_2022;
+import aOc22.Day25_2022;
 
 public class Handler {
 	private int day;
@@ -255,194 +279,194 @@ public class Handler {
 			}
 			else if (day==2) {
 				if (part==1) {
-//					ris=Day02_2022.calculate1(inp);
+					ris=Day02_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day02_2022.calculate2(inp);
+					ris=Day02_2022.calculate2(inp);
 				}
 			}
 			else if (day==3) {
 				if (part==1) {
-//					ris=Day03_2022.calculate1(inp);
+					ris=Day03_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day03_2022.calculate2(inp);
+					ris=Day03_2022.calculate2(inp);
 				}
 			}
 			else if (day==4) {
 				if (part==1) {
-//					ris=Day04_2022.calculate1(inp);
+					ris=Day04_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day04_2022.calculate2(inp);
+					ris=Day04_2022.calculate2(inp);
 				}
 			}
 			else if (day==5) {
 				if (part==1) {
-//					ris=Day05_2022.calculate1(inp);
+					ris=Day05_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day05_2022.calculate2(inp);
+					ris=Day05_2022.calculate2(inp);
 				}
 			}
 			else if (day==6) {
 				if (part==1) {
-//					ris=Day06_2022.calculate1(inp);
+					ris=Day06_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day06_2022.calculate2(inp);
+					ris=Day06_2022.calculate2(inp);
 				}
 			}
 			else if (day==7) {
 				if (part==1) {
-//					ris=Day07_2022.calculate1(inp);
+					ris=Day07_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day07_2022.calculate2(inp);
+					ris=Day07_2022.calculate2(inp);
 				}
 			}
 			else if (day==8) {
 				if (part==1) {
-//					ris=Day08_2022.calculate1(inp);
+					ris=Day08_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day08_2022.calculate2(inp);
+					ris=Day08_2022.calculate2(inp);
 				}
 			}
 			else if (day==9) {
 				if (part==1) {
-//					ris=Day09_2022.calculate1(inp);
+					ris=Day09_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day09_2022.calculate2(inp);
+					ris=Day09_2022.calculate2(inp);
 				}
 			}
 			else if (day==10) {
 				if (part==1) {
-//					ris=Day10_2022.calculate1(inp);
+					ris=Day10_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day10_2022.calculate2(inp);
+					ris=Day10_2022.calculate2(inp);
 				}
 			}
 			else if (day==11) {
 				if (part==1) {
-//					ris=Day11_2022.calculate1(inp);
+					ris=Day11_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day11_2022.calculate2(inp);
+					ris=Day11_2022.calculate2(inp);
 				}
 			}
 			else if (day==12) {
 				if (part==1) {
-//					ris=Day12_2022.calculate1(inp);
+					ris=Day12_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day12_2022.calculate2(inp);
+					ris=Day12_2022.calculate2(inp);
 				}
 			}
 			else if (day==13) {
 				if (part==1) {
-//					ris=Day13_2022.calculate1(inp);
+					ris=Day13_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day13_2022.calculate2(inp);
+					ris=Day13_2022.calculate2(inp);
 				}
 			}
 			else if (day==14) {
 				if (part==1) {
-//					ris=Day14_2022.calculate1(inp);
+					ris=Day14_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day14_2022.calculate2(inp);
+					ris=Day14_2022.calculate2(inp);
 				}
 			}
 			else if (day==15) {
 				if (part==1) {
-//					ris=Day15_2022.calculate1(inp);
+					ris=Day15_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day15_2022.calculate2(inp);
+					ris=Day15_2022.calculate2(inp);
 				}
 			}
 			else if (day==16) {
 				if (part==1) {
-//					ris=Day16_2022.calculate1(inp);
+					ris=Day16_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day16_2022.calculate2(inp);
+					ris=Day16_2022.calculate2(inp);
 				}
 			}
 			else if (day==17) {
 				if (part==1) {
-//					ris=Day17_2022.calculate1(inp);
+					ris=Day17_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day17_2022.calculate2(inp);
+					ris=Day17_2022.calculate2(inp);
 				}
 			}
 			else if (day==18) {
 				if (part==1) {
-//					ris=Day18_2022.calculate1(inp);
+					ris=Day18_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day18_2022.calculate2(inp);
+					ris=Day18_2022.calculate2(inp);
 				}
 			}
 			else if (day==19) {
 				if (part==1) {
-//					ris=Day19_2022.calculate1(inp);
+					ris=Day19_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day19_2022.calculate2(inp);
+					ris=Day19_2022.calculate2(inp);
 				}
 			}
 			else if (day==20) {
 				if (part==1) {
-//					ris=Day20_2022.calculate1(inp);
+					ris=Day20_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day20_2022.calculate2(inp);
+					ris=Day20_2022.calculate2(inp);
 				}
 			}
 			else if (day==21) {
 				if (part==1) {
-//					ris=Day21_2022.calculate1(inp);
+					ris=Day21_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day21_2022.calculate2(inp);
+					ris=Day21_2022.calculate2(inp);
 				}
 			}
 			else if (day==22) {
 				if (part==1) {
-//					ris=Day22_2022.calculate1(inp);
+					ris=Day22_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day22_2022.calculate2(inp);
+					ris=Day22_2022.calculate2(inp);
 				}
 			}
 			else if (day==23) {
 				if (part==1) {
-//					ris=Day23_2022.calculate1(inp);
+					ris=Day23_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day23_2022.calculate2(inp);
+					ris=Day23_2022.calculate2(inp);
 				}
 			}
 			else if (day==24) {
 				if (part==1) {
-//					ris=Day24_2022.calculate1(inp);
+					ris=Day24_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day24_2022.calculate2(inp);
+					ris=Day24_2022.calculate2(inp);
 				}
 			}
 			else if (day==25) {
 				if (part==1) {
-//					ris=Day25_2022.calculate1(inp);
+					ris=Day25_2022.calculate1(inp);
 				}
 				else if (part==2) {
-//					ris=Day25_2022.calculate2(inp);
+					ris=Day25_2022.calculate2(inp);
 				}
 			}
 		}
