4b3c6414a90bf027c4ff63ea9d4d7153e8aa75b8
Pucchosa <pucchosa@proton.me>
2022-11-24 09:28:32 +0100

    get ready for 2022

---
+++ src/aOc22/Day18_2021.java
@@ -0,0 +1,20 @@
+package aOc22;
+
+public class Day18_2021 {
+	public static long calculate1(String s) {
+		int fin=0;
+		
+		Nope no=new Nope();
+		no.setVisible(true);
+		
+		return (long) fin;
+	}
+	public static long calculate2(String s) {
+		int fin=0;
+		
+		Nope no=new Nope();
+		no.setVisible(true);
+		
+		return (long) fin;
+	}
+}
