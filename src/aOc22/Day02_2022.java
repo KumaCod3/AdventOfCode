f9e66933528f09fc65283cc8f3ea6c4ef4f4f9dd
Pucchosa <pucchosa@proton.me>
2022-12-02 06:25:13 +0100

    2022 Day2

--- src/aOc22/Day02_2022.java
+++ src/aOc22/Day02_2022.java
@@ -1,22 +1,89 @@
 package aOc22;
-
-import program.Nope;
+import java.util.ArrayList;
+import program.Read;
 
 public class Day02_2022 {
 	public static long calculate1(String s) {
+		ArrayList<String[]> lista = Read.copList(s);
 		int fin=0;
 		
-		Nope no=new Nope("Sorry, develop time travel is way beyond my capacity!");
-		no.setVisible(true);
-		
+		for (String[] a: lista) {
+			if (a[0].equals("A")) {
+				if (a[1].equals("X")) {
+					fin=fin+(3+1);
+				}
+				else  if (a[1].equals("Y")) {
+					fin=fin+(6+2);
+				}
+				else  if (a[1].equals("Z")) {
+					fin=fin+(0+3);
+				}
+			}
+			else if (a[0].equals("B")) {
+				if (a[1].equals("X")) {
+					fin=fin+(0+1);
+				}
+				else  if (a[1].equals("Y")) {
+					fin=fin+(3+2);
+				}
+				else  if (a[1].equals("Z")) {
+					fin=fin+(6+3);
+				}
+			}
+			else if (a[0].equals("C")) {
+				if (a[1].equals("X")) {
+					fin=fin+(6+1);
+				}
+				else  if (a[1].equals("Y")) {
+					fin=fin+(0+2);
+				}
+				else  if (a[1].equals("Z")) {
+					fin=fin+(3+3);
+				}
+			}
+		}
+		System.out.println("OOOOK");
 		return (long) fin;
 	}
 	public static long calculate2(String s) {
+		ArrayList<String[]> lista = Read.copList(s);
 		int fin=0;
 		
-		Nope no=new Nope("Sorry, develop time travel is way beyond my capacity!");
-		no.setVisible(true);
-		
+		for (String[] a: lista) {
+			if (a[0].equals("A")) {
+				if (a[1].equals("X")) {
+					fin=fin+(0+3);
+				}
+				else  if (a[1].equals("Y")) {
+					fin=fin+(3+1);
+				}
+				else  if (a[1].equals("Z")) {
+					fin=fin+(6+2);
+				}
+			}
+			else if (a[0].equals("B")) {
+				if (a[1].equals("X")) {
+					fin=fin+(0+1);
+				}
+				else  if (a[1].equals("Y")) {
+					fin=fin+(3+2);
+				}
+				else  if (a[1].equals("Z")) {
+					fin=fin+(6+3);
+				}
+			}
+			else if (a[0].equals("C")) {
+				if (a[1].equals("X")) {
+					fin=fin+(0+2);
+				}
+				else  if (a[1].equals("Y")) {
+					fin=fin+(3+3);
+				}
+				else  if (a[1].equals("Z")) {
+					fin=fin+(6+1);
+				}
+			}
+		}
 		return (long) fin;
 	}
 }
