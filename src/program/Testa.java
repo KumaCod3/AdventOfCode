e7b4616523d3339d7cc69b400a60209b9a941a68
mixim <mixim@pCla.fritz.box>
2023-02-24 17:16:30 +0100

    linked logo

--- src/program/Testa.java
+++ src/program/Testa.java
@@ -1,11 +1,17 @@
 package program;
-
 import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Desktop;
 import java.awt.FlowLayout;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -18,9 +24,22 @@ public class Testa extends JPanel {
 		pan.setOpaque(false);
 		add(pan);
 		JLabel tz=new JLabel();
-		tz.setText("Powered by Pucchosa.sa ");
+		tz.setText("Powered by Pucchosa ");
 		tz.setForeground(Color.GREEN);
 		tz.setOpaque(false);
+		tz.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		tz.addMouseListener(new MouseAdapter() {
+		    @Override
+		    public void mouseClicked(MouseEvent e) {
+		    	try {
+		    		 Desktop.getDesktop().browse(new URI("https://github.com/Pucchosa"));
+		    	 } catch (IOException | URISyntaxException e1) {}
+		    }
+		    @Override
+		    public void mouseEntered(MouseEvent e) {}
+		    @Override
+		    public void mouseExited(MouseEvent e) {}
+		});
 		add(tz);
 	}
 	
