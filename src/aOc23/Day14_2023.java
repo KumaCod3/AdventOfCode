1fbcd4dec289608b10e052a378cdff1fbf7a6dbc
KumaCod3 <kumacod3@proton.me>
2023-12-13 07:44:09 +0100

    prova

---
+++ src/aOc23/Day14_2023.java
@@ -0,0 +1,20 @@
+package aOc23;
+
+import java.util.ArrayList;
+
+import program.Read;
+
+public class Day14_2023 {
+	public static long calculate1(String s) {
+		ArrayList<String[]> lista = Read.copList23(s);
+		int fin=0;
+		
+		return (long) fin;
+	}
+	public static long calculate2(String s) {
+		ArrayList<String[]> lista = Read.copList23(s);
+		int fin=0;
+		
+		return (long) fin;
+	}
+}
